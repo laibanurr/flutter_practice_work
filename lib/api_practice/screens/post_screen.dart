df5ce05e5a9b1ef5e527b85f43a67c6716f8f7cc
Laiba nurr <laibasirahh7@gmail.com>
2026-08-03 21:30:04 +0500

    made a ui for post class

---
+++ lib/api_practice/screens/post_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_flutter/api_practice/models/post.dart';
+import 'package:navigation_flutter/api_practice/services/api_service.dart';
+
+class PostScreen extends StatelessWidget {
+  const PostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        foregroundColor: Colors.white,
+       title : Text('Posts', style: TextStyle(fontSize: 24,
+        fontWeight: FontWeight.bold),
+       ),
+       centerTitle: false,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+        IconButton(onPressed: (){}, icon: Icon(Icons.favorite_outline))],
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: fetchPosts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error , ${snapshot.error}'));
+          }
+          final posts = snapshot.data!;
+          return ListView.builder(
+            itemCount: posts.length,
+
+            itemBuilder: (context, index) {
+              final post = posts[index];
+              return Card(
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Text(
+                        '${post.id}. ${post.title}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      Text(post.body,
+                      style: TextStyle(
+                        color: Colors.grey,
+                          fontSize: 12,
+                      ),)
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
