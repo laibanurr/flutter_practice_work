b594238ddce10a90e0e86c61272216f65d76d976
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-06 20:02:27 +0500

    feat: refactor authentication screens and routes for improved structure and navigation

--- lib/auth/models/routes/go_routers.dart
+++ lib/auth/models/routes/go_routers.dart
@@ -1,14 +1,14 @@
 import 'package:go_router/go_router.dart';
-import 'package:navigation_flutter/screens/log_in_screen.dart';
-import 'package:navigation_flutter/screens/profile_screen.dart';
+import 'package:navigation_flutter/auth/screens/login_screen.dart';
+import 'package:navigation_flutter/auth/screens/profile_screen.dart';
 
-final GoRouter _authRoutes = GoRouter(
+final GoRouter authRoutes = GoRouter(
   initialLocation: '/userloginscreen',
   routes: [
     GoRoute(
       path: '/userloginscreen',
-      builder: (context, state) => const LoginScreen(),
+      builder: (context, state) => const UserLoginScreen(),
     ),
-    GoRoute(path: '/userprofilescreen' , builder: (context, state)=>ProfileScreen())
+    GoRoute(path: '/userprofilescreen' , builder: (context, state)=>UserProfileScreen())
   ],
 );
