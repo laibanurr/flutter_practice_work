5126ff3336d23f9c319a90bc08df3d076791a567
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-06 20:06:19 +0500

    changed my main to apply go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation_flutter/api_practice/screens/post_screen.dart';
+import 'package:navigation_flutter/auth/models/routes/go_routers.dart';
 import 'package:navigation_flutter/auth/screens/login_screen.dart';
 import 'package:navigation_flutter/dio_api_practice/models/routes/go_routers.dart';
 import 'package:navigation_flutter/dio_api_practice/screens/user_screen.dart';
@@ -18,11 +19,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    //  return MaterialApp.router(routerConfig: appRouterApi ,
-     return MaterialApp(
-       home: LoginScreen(),
+     return MaterialApp.router(routerConfig:authRoutes ,
+    //  return MaterialApp(
+    //    home: LoginScreen(),
     
-      debugShowCheckedModeBanner: false,
+    //   debugShowCheckedModeBanner: false,
       // initialRoute: '/login',
       // routes: {
       //   '/login': (context) => const LoginScreen(),
