37056b0baa04f53e92a28142617ca9cea111c2af
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-07 19:34:03 +0500

    feat: update GoRouter configuration for user login and profile screens with AuthService integration

--- lib/auth/models/routes/go_routers.dart
+++ lib/auth/models/routes/go_routers.dart
@@ -1,14 +1,22 @@
+// in your go_router file
 import 'package:go_router/go_router.dart';
 import 'package:navigation_flutter/auth/screens/login_screen.dart';
 import 'package:navigation_flutter/auth/screens/profile_screen.dart';
+import 'package:navigation_flutter/auth/services/auth_service.dart';
 
-final GoRouter authRoutes = GoRouter(
-  initialLocation: '/userloginscreen',
+final authService = AuthService(); // ONE instance, created once
+
+final GoRouter authRouterApi = GoRouter(
+  initialLocation: '/login',
   routes: [
     GoRoute(
-      path: '/userloginscreen',
-      builder: (context, state) => const UserLoginScreen(),
+      path: '/login',
+      builder: (context, state) =>
+      UserLoginScreen(authservice: authService)
     ),
-    GoRoute(path: '/userprofilescreen' , builder: (context, state)=>UserProfileScreen())
+    GoRoute(
+      path: '/userprofilescreen',
+      builder: (context, state) => UserProfileScreen(authService: authService),
+    ),
   ],
 );
