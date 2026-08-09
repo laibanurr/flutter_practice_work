4810c139c821807a3fe80d7b3e16a6b2544521cb
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-09 20:43:45 +0500

    feat: refactor main.dart to streamline imports and update app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:navigation_flutter/api_practice/screens/post_screen.dart';
-import 'package:navigation_flutter/auth/models/routes/go_routers.dart';
-import 'package:navigation_flutter/auth/screens/login_screen.dart';
-import 'package:navigation_flutter/dio_api_practice/models/routes/go_routers.dart';
-import 'package:navigation_flutter/dio_api_practice/screens/user_screen.dart';
-import 'package:navigation_flutter/dio_api_practice/screens/user_screen_interceptors.dart';
-import 'package:navigation_flutter/stylingwidgets/popupmenu/simple_pop_up_menu.dart';
-import 'package:navigation_flutter/textformfield/contact_form.dart';
-import 'package:provider/provider.dart';
-import 'provider/user_provider.dart';
 
-void main() {
-  runApp(
-    ChangeNotifierProvider(create: (_) => UserProvider(), child: const MyApp()),
-  );
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:navigation_flutter/auth/models/routes/go_routers.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await authService.loadToken();
+  // runApp(
+  //   ChangeNotifierProvider(create: (_) => UserProvider(), child: const MyApp()),
+  // );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    //  return MaterialApp.router(routerConfig:authRouterApi ,
-     return MaterialApp(
-       home: SimplePopUpMenu(),
-    
-       debugShowCheckedModeBanner: false,
+    return MaterialApp.router(
+      routerConfig: authRouterApi,
+
+      //return MaterialApp(
+      //  home: PopupMenuWithBottomShhet(),
+      debugShowCheckedModeBanner: false,
       // initialRoute: '/login',
       // routes: {
       //   '/login': (context) => const LoginScreen(),
@@ -38,4 +36,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
