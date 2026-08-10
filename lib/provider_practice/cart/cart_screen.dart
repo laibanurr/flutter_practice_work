02dab52220e63ef7b4e609d5459fafd7f2894d5c
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-10 17:05:02 +0500

    feat: add CartScreen for displaying items in the cart

---
+++ lib/provider_practice/cart/cart_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_flutter/provider_practice/cart/cart_model.dart';
+import 'package:provider/provider.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = context.watch<CartModel>();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Cart'),
+      ),
+      body: ListView.builder(
+        itemCount: cart.items.length,
+        itemBuilder: (context , index)=>ListTile(
+          title: Text("${cart.items[index]}"),
+        )
+        ),
+    );
+  }
+}
