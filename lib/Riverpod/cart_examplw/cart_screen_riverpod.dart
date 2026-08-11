8a8faba5f1289bd609ee16b1cc6f983179418a29
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-11 20:19:02 +0500

    fix: restructure CartScreenRiverpod layout to display total cart items

--- lib/Riverpod/cart_examplw/cart_screen_riverpod.dart
+++ lib/Riverpod/cart_examplw/cart_screen_riverpod.dart
@@ -8,11 +8,20 @@ class CartScreenRiverpod extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final totalCartItems = ref.watch(cartProvider);
+    final total = ref.watch(totalCartItemsProvider);
     return Scaffold(
-      body: ListView.builder(
-        itemCount: totalCartItems.length,
-        itemBuilder: (context, index) =>
-            ListTile(title: Text(totalCartItems[index])),
+      body: Column(
+        children: [
+          Expanded(child: 
+          ListView.builder(
+            itemCount: totalCartItems.length,
+            itemBuilder: (context, index) =>
+                ListTile(title: Text(totalCartItems[index]))
+          ),
+          ),
+          SizedBox(height: 10,),
+           Text('Total Cart Items : \$$total')
+        ]
       ),
     );
   }
