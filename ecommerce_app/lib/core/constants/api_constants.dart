f170f9d8ed398c476e683bd743c638b27f7c0c14
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-13 15:42:25 +0500

    feat: add ApiConstants class for API endpoint management

---
+++ ecommerce_app/lib/core/constants/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  static const String baseUrl = 'https://dummyjson.com';
+  static const String loginUrl = '/auth/login';
+  static const String productsUrl = '/products';
+  static const String cartUrl = '/cart';
+}
