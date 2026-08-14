16733ea07e261a4fbec2fadb84cc586492f7e725
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-14 17:55:43 +0500

    feat: implement AuthService for user authentication and error handling

---
+++ ecommerce_app/lib/features/services/auth_service.dart
@@ -0,0 +1,49 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/core/constants/api_constants.dart';
+import 'package:ecommerce_app/core/network/dio_client.dart';
+import 'package:ecommerce_app/core/storage/secure_storage.dart';
+import 'package:ecommerce_app/features/models/auth_model.dart';
+
+class AuthService {
+  final _dio = DioClient().dio;
+  final _storage = SecureStorage();
+  String? accessToken;
+
+  Future<AuthData> login(String username, String password) async {
+    try {
+      final response = await _dio.post(
+        ApiConstants.loginUrl,
+        data: {'username': username, 'password': password},
+      );
+      final authData = AuthData.fromJson(response.data);
+      accessToken = authData.accessToken;
+      await _storage.saveAccessToken(authData.accessToken);
+      return authData;
+    } on DioException catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<void> logout() async {
+    accessToken = null;
+    await _storage.deleteToken();
+  }
+}
+
+Exception _handleError(DioException e) {
+  switch (e.type) {
+    case DioExceptionType.connectionTimeout:
+      return Exception('Connection timed out');
+    case DioExceptionType.receiveTimeout:
+      return Exception('Server took too long');
+    case DioExceptionType.badResponse:
+      final code = e.response?.statusCode;
+      if (code == 401) return Exception('Wrong username or password');
+      if (code == 404) return Exception('User not found');
+      return Exception('Server error: $code');
+    case DioExceptionType.connectionError:
+      return Exception('No internet connection');
+    default:
+      return Exception('Something went wrong');
+  }
+}
