ecb1146cf2dba80102d5354348ad93b905f457d3
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-14 19:26:54 +0500

    feat: update routing to include login, products, and cart screens with authentication handling

--- ecommerce_app/lib/routers.dart
+++ ecommerce_app/lib/routers.dart
@@ -1,25 +1,41 @@
-import 'package:ecommerce_app/products/model/product_model.dart';
+
+import 'package:ecommerce_app/cart/screen/cart_screen.dart';
+import 'package:ecommerce_app/features/screens/login_screen.dart';
+import 'package:ecommerce_app/features/services/auth_service.dart';
 import 'package:ecommerce_app/products/screens/product_detail_screen.dart';
 import 'package:ecommerce_app/products/screens/products_screen.dart';
 import 'package:go_router/go_router.dart';
 
-final GoRouter appRouter = GoRouter(
-  initialLocation: '/',
+final authService = AuthService();
+
+final GoRouter router = GoRouter(
+  initialLocation: '/login',
+  redirect: (context, state) {
+    final isLoggedIn = authService.accessToken != null;
+    final isOnLogin = state.matchedLocation == '/login';
+    if (isLoggedIn && isOnLogin) return '/products';
+    if (!isLoggedIn && !isOnLogin) return '/login';
+    return null;
+  },
   routes: [
     GoRoute(
-      path: '/',
-      name: 'products',
+      path: '/login',
+      builder: (context, state) => LoginScreen(authService: authService),
+    ),
+    GoRoute(
+      path: '/products',
+      builder: (context, state) => const ProductsScreen(),
+    ),
+    GoRoute(
+      path: '/product/:id',
       builder: (context, state) {
-        return const ProductScreen();
+        final id = int.parse(state.pathParameters['id']!);
+        return ProductDetailScreen(productId: id);
       },
     ),
     GoRoute(
-      path: '/details',
-      name: 'details',
-      builder: (context, state) {
-        final Product selectedProduct = state.extra as Product;
-        return ProductDetailScreen(product: selectedProduct);
-      },
+      path: '/cart',
+      builder: (context, state) => const CartScreen(),
     ),
   ],
 );
