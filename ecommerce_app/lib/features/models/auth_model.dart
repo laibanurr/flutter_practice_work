31ade2a5ae1ca4dd6585f2606f6dfaae4fe3be70
Laiba nurr <ID+username@users.noreply.github.com>
2026-08-14 17:32:40 +0500

    feat: add AuthData model for managing authentication data

---
+++ ecommerce_app/lib/features/models/auth_model.dart
@@ -0,0 +1,37 @@
+class AuthData {
+  final String accessToken;
+  final String refreshToken;
+  final int id;
+  final String username;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String gender;
+  final String image;
+
+  AuthData({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+  });
+
+  factory AuthData.fromJson(Map<String, dynamic> json) {
+    return AuthData(
+      accessToken: json['accessToken'],
+      refreshToken: json['refreshToken'],
+      id: json['id'],
+      username: json['username'],
+      email: json['email'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      gender: json['gender'],
+      image: json['image'],
+    );
+  }
+}
